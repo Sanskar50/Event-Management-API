d7a3d7da63ce1c7e046bbae60145abfefc2808b0
Sanskar Suri <sanskarsuri50@gmail.com>
2024-03-28 20:20:24 +0530

    removed the mongodb password and username in url

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const mongoclient = mongodb.MongoClient;
 let _db;
 
 const mongoconnect = (cb) => {
-    mongoclient.connect('mongodb+srv://mr-ss:OqPlVe8nWQieZ65s@cluster0.rwwbhzq.mongodb.net/eventsDB?retryWrites=true&w=majority&appName=Cluster0')
+    mongoclient.connect('<username>:<password>@cluster0.rwwbhzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         .then(client => {
             console.log('Connected!');
             _db=client.db();
--- util/inputData.js
+++ util/inputData.js
@@ -9,7 +9,7 @@ const collectionName = 'events'; // Collection name
 const events = [];
 
 
-fs.createReadStream(path.join(__dirname, 'Backend_assignment_gg_dataset - dataset.csv'))
+fs.createReadStream(path.join(__dirname, 'dummy- dataset.csv'))
     .pipe(csv())
     .on('data', async (row) => {
         try {
